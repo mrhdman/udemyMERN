d0e57243ca2c2ef476966706f6253f83720e50b9
mrhdman <mattrhavins@gmail.com>
2019-05-15 13:59:59 -0400

    delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -144,4 +144,22 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user and posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove user's posts
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'user deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
